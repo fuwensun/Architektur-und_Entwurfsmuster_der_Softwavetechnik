7a22f7abe21f03b9db0b70d70f5279f487962c9c
sfw <u18a@sfw.com>
2018-05-03 09:37:12 +0800

    --fea-adapter--

--- Adapter/src/main/java/TestAdapter.java
+++ Adapter/src/main/java/TestAdapter.java
@@ -3,9 +3,7 @@ import java.util.Vector;
 public class TestAdapter {
 
     public static void main(String[] args) {
-
         System.out.println("test!!!");
-
         IPersonenLeser leser = new CSVLeserAdapter("xxx.csv");
         Vector<Person> personen = leser.lesePersonen();
 
