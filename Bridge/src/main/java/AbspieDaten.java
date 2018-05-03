ef137a941c0ea448951ade27d12f135da2178dba
sfw <u18a@sfw.com>
2018-05-03 20:48:17 +0800

    --add AbspieDaten--

---
+++ Bridge/src/main/java/AbspieDaten.java
@@ -0,0 +1,13 @@
+//播放数据
+
+public class AbspieDaten {
+    private String daten;
+
+    public AbspieDaten(String daten){
+        this.daten = daten;
+    }
+
+    public String toString(){
+        return daten;
+    }
+}
