e9f4d7f3c02f27a9d4149a42e4c6ce71076e9b5e
sfw <u18a@sfw.com>
2018-05-03 14:52:32 +0800

    --add comparison--

--- Adapter/src/main/java/TestAdapter.java
+++ Adapter/src/main/java/TestAdapter.java
@@ -4,8 +4,17 @@ public class TestAdapter {
 
     public static void main(String[] args) {
         System.out.println("test!!!");
+
+        //低耦合，用IPersonenLeser接口，CSVLeserAdapter混入接口的目的，面向接口编程，ok!ok!
         IPersonenLeser leser = new CSVLeserAdapter("xxx.csv");
         Vector<Person> personen = leser.lesePersonen();
+
+        for(Person person : personen)
+            person.print();
+
+        //高耦合，TestAdapter类依赖CSVLeserAdapter类，虽然适配的目的达到了，面向实现编程XXXX
+        CSVLeserAdapter CSVleser = new CSVLeserAdapter("xxx.csv");
+        personen = leser.lesePersonen();
 
         for(Person person : personen)
             person.print();
