470a938ebe16cfda9065960c3081e0237c60b073
sfw <u18a@sfw.com>
2018-05-03 21:04:29 +0800

    --add Abspielgeraet--

---
+++ Bridge/src/main/java/Abspielgeraet.java
@@ -0,0 +1,21 @@
+//播放设备
+
+public class Abspielgeraet {
+
+    protected IAbspielgeraetImplementierer impl;
+
+    public Abspielgeraet(IAbspielgeraetImplementierer impl){
+        this.impl = impl;
+    }
+
+    public void spieleAb(int liedNummer){
+        impl.springeZuTrack(liedNummer);
+        AbspieDaten dat = impl.leseDaten();
+        //数据输出
+        System.out.println(dat);
+    }
+
+    public void ausschalten(){
+        impl.ausschalten();
+    }
+}
